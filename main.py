20f6874b06229525487d744b4750ed5cfbf7c6df
ashu7mishra <ashu7mishra@gmail.com>
2025-06-16 07:45:47 +0530

    added patient endpoint

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 import json
 
 app = FastAPI()
@@ -11,12 +11,21 @@ def load_data():
 
 @app.get("/")
 def hello():
-    return {"message: ": "Patient management system API"}
+    return {"message: ": "Patient management system API."}
 
 @app.get("/about")
 def about():
-    return {"message: ": "A fully functional API to manage  your patient records"}
+    return {"message: ": "A fully functional API to manage  your patient records."}
 
 @app.get("/view")
 def view():
     return load_data()
+
+@app.get("/patient/{patient_id}/")
+def view_patient(patient_id: str):
+    data = load_data()
+    
+    if patient_id in data:
+        return data[patient_id]
+    return {"message:": "Patient not found"}
+    
